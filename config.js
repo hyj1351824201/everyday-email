19ca72cb9be626926bfe258fc438e269b91e5e7f
huang.yunjin <1351824201@qq.com>
2021-10-12 09:21:59 +0800

    1

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
   user: '1351824201@qq.com', // 发送者邮箱
   pass: 'jvfsplcxafwtiadc', // 发送者邮箱MTP协议密码
   to: '1351824201@qq.com', // 发送到谁，填邮箱
-  weatherKey: '33369e365fe84eb68876f52a2ae51cca', // 和风天气key
-  location: '101210102',  // 和风天气-成都武侯区的id
+  weatherKey: '6454eadfd79a4f8ca827828d27311bc9', // 和风天气key
+  location: '101210101',  // 和风天气-locationId  https://geoapi.qweather.com/v2/city/lookup?location=杭州&key=6454eadfd79a4f8ca827828d27311bc9
   type: '1,3,9', // 和风天气-生活指数type
   tianXingKey: 'eb75297818f2413f24e1f1f76662bccd', // 天行数据的key
   startDay: '2021-08-01', // 在一起的日期
